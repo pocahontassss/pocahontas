51f3525f8ff3d5244c6d3b8739715f1e3e1379a1
pocahontassss <babenkovlds@gmail.com>
2024-04-30 23:33:00 +0300

    сделал меню работоспособным + добавил модалку о добавлении товара в корзину
    Честно говоря js дается пока что оооочень тяжело, сделал модалку с помощью chat gpt и то с трудом...

--- source/js/script.js
+++ source/js/script.js
@@ -1,40 +1,38 @@
-let burger  = document.querySelectorAll('.js-burger');
-let menu    = document.querySelector('.burger-panel');
-let burgerButton  = document.querySelector('.burger');
-let body  = document.querySelector('body');
+const headerOpenMenu = document.querySelector('.header__burger');
+const headerMenu = document.querySelector ('.header__nav');
+const headerClosedMenu = document.querySelector('.header__closed');
 
-let i;
-for (i = 0; i < burger.length; i++) {
-  burger[i].onclick = function() {
-    menu.classList.toggle("active");
-    burgerButton.classList.toggle("burger--open");
-    body.classList.toggle("body-lock");
-  }
+const HeaderCloseButton = () => {
+  headerMenu.classList.remove ('header__nav--showed');
+  headerClosedMenu.removeEventListener('click', HeaderCloseButton);
 };
 
-window.onscroll = function() {myFunction()};
-let headerGo = document.querySelector('.header');
-let main = document.querySelector('main');
-let sticky = headerGo.offsetTop;
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    headerGo.classList.add("header--sticky");
-    main.classList.add("main");
-  } else {
-    headerGo.classList.remove("header--sticky");
-    main.classList.remove("main");
-  }
-}
+headerOpenMenu.addEventListener('click', () => {
+  headerMenu.classList.add('header__nav--showed');
+  headerClosedMenu.addEventListener('click', HeaderCloseButton); 
+});
 
-let acc = document.getElementsByClassName("tab__button");
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("tab__button--open");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+
+const productAdds = document.querySelectorAll('.product-card__arrow-icon');
+const modalProductAdd = document.querySelector('.modal__addProduct');
+const modalProductClose = document.querySelector('.modal__addProduct-buttonClose');
+const modalContinue = document.querySelector('.modal__addProduct-button');
+
+const modalClose = () => {
+  modalProductAdd.classList.remove('modal__addProduct--showed');
+  modalProductClose.removeEventListener('click', modalClose);
+};
+
+const modalContinueButton = () => {
+  modalProductAdd.classList.remove('modal__addProduct--showed');
+  modalProductClose.removeEventListener('click', modalContinueButton);
+};
+
+productAdds.forEach(function(productAdd) {
+  productAdd.addEventListener('click', () => {
+    modalProductAdd.classList.add('modal__addProduct--showed');
+    modalProductClose.addEventListener('click', modalClose);
+    modalContinue.addEventListener('click', modalContinueButton);
   });
-}
+});
+
