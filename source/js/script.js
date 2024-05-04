45522394c70ef773218eec17f05d488823148f8c
pocahontassss <babenkovlds@gmail.com>
2024-05-04 22:49:48 +0300

    Обновил модалки
    1. Добавил скрипт для вывода модального окна после нажатия на кнопку "подписаться"
    2. Сделал модалки под разные разрешения экрана.
    3. Сделал БЭМ-блок для модалки и вынес общие свойства для них

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// Функция для открытия меню на мобилке
+
 const headerOpenMenu = document.querySelector('.header__burger');
 const headerMenu = document.querySelector ('.header__nav');
 const headerClosedMenu = document.querySelector('.header__closed');
@@ -12,26 +14,44 @@ headerOpenMenu.addEventListener('click', () => {
   headerClosedMenu.addEventListener('click', HeaderCloseButton); 
 });
 
+// Модальное окно об добавлении продуктов в корзину
 
 const productAdds = document.querySelectorAll('.product-card__arrow-icon');
-const modalProductAdd = document.querySelector('.modal__addProduct');
-const modalProductClose = document.querySelector('.modal__addProduct-buttonClose');
-const modalContinue = document.querySelector('.modal__addProduct-button');
+const modalProductAdd = document.querySelector('.modal__add-product');
+const modalProductClose = document.querySelector('.modal__button-close');
+const modalContinue = document.querySelector('.modal__button');
 
 const modalClose = () => {
-  modalProductAdd.classList.remove('modal__addProduct--showed');
+  modalProductAdd.classList.remove('modal__container--showed');
   modalProductClose.removeEventListener('click', modalClose);
 };
 
 const modalContinueButton = () => {
-  modalProductAdd.classList.remove('modal__addProduct--showed');
+  modalProductAdd.classList.remove('modal__container--showed');
   modalProductClose.removeEventListener('click', modalContinueButton);
 };
 
 productAdds.forEach(function(productAdd) {
   productAdd.addEventListener('click', () => {
-    modalProductAdd.classList.add('modal__addProduct--showed');
+    modalProductAdd.classList.add('modal__container--showed');
     modalProductClose.addEventListener('click', modalClose);
     modalContinue.addEventListener('click', modalContinueButton);
   });
+});
+
+// Модальное окно - проверка подписки, email
+
+const modalEmailOpen = document.querySelector('.footer__button-subscribe');
+const modalEmailClose = document.querySelector('.modal__email .modal__button-close');
+const modalEmail = document.querySelector('.modal__email');
+
+const EmailCloseButton = () => {
+  modalEmail.classList.remove('modal__container--showed');
+  modalEmailClose.removeEventListener ('click', EmailCloseButton);
+};
+
+modalEmailOpen.addEventListener('click', (event) => {
+  event.preventDefault();
+  modalEmail.classList.add('modal__container--showed');
+  modalEmailClose.addEventListener('click', EmailCloseButton);
 });
